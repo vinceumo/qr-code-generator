505ba59949d8368a62e16075008899b87f3e81c3
Vincent Humeau <hello@vincent-humeau.com>
2019-05-27 17:46:40 +0100

    Added icons and update manifest

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -5,6 +5,9 @@ function Header() {
     <div className="has-bg-dark">
       <div className="container has-py-2 has-mb-3">
         <h1>QR Code Generator</h1>
+        <p>
+          <a href="https://github.com/vinceumo/qr-code-generator">See project on github</a>
+        </p>
       </div>
     </div>
   );
