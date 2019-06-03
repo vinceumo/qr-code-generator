9097a625ca1b57896c4ce63e58e62f7c7a147133
Vincent Humeau <hello@vincent-humeau.com>
2019-06-03 17:00:44 +0100

    Added router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Header from './Components/Organisms/Header'
 import QrCodeGenerator from "./Components/Organisms/QrCodeGenerator"
+import QrCodeScanner from "./Components/Organisms/QrCodeScanner";
+
 
 function App() {
   return (
-    <div>
-      <Header />
-      <QrCodeGenerator />
-    </div>
+    <Router>
+      <div>
+        <Header />
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Generator</Link>
+            </li>
+            <li>
+              <Link to="/scanner/">Scanner</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={QrCodeGenerator} />
+        <Route path="/scanner/" component={QrCodeScanner} />
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Organisms/QrCodeScanner.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function QrCodeScanner() {
+  return <p>Hello form QrCodeScanner</p>;
+}
+
+export default QrCodeScanner;
