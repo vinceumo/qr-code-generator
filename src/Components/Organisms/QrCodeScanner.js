f8538df58a6166e3d0385ea692c5c0165c43fa4b
Vincent Humeau <hello@vincent-humeau.com>
2019-06-05 16:42:22 +0100

    Added basic scanner

--- src/Components/Organisms/QrCodeScanner.js
+++ src/Components/Organisms/QrCodeScanner.js
@@ -1,7 +1,74 @@
-import React from "react";
+import React, {useState, useEffect, useRef } from "react";
+import jsQR from "jsqr";
 
 function QrCodeScanner() {
-  return <p>Hello form QrCodeScanner</p>;
+  const [msg, setMsg] = useState("");
+  const videoRef = useRef(null);
+  const canvasRef = useRef(null);
+  const constraints = { audio: false, video: true };
+  let canvas;
+
+  function imageData() {
+    if(!canvas) {
+      canvas = canvasRef.current.getContext("2d");
+    }
+
+    canvasRef.current.height = videoRef.current.clientHeight;
+    canvasRef.current.width = videoRef.current.clientWidth;
+
+    canvas.drawImage(
+      videoRef.current,
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+
+    var image = canvas.getImageData(
+      0,
+      0,
+      canvasRef.current.width,
+      canvasRef.current.height
+    );
+
+    return image;
+  }
+
+  function checkFrame() {
+    const image = imageData();
+    var code = jsQR(image.data, image.width, image.height, {
+      inversionAttempts: "dontInvert"
+    });
+
+    console.log(code);
+
+    if (code) {
+      console.log(code.data);
+      setMsg(code.data);
+    }
+    requestAnimationFrame(checkFrame);
+  }
+
+  function successStream(stream) {
+    videoRef.current.srcObject = stream;
+    requestAnimationFrame(checkFrame);
+  }
+
+  function failureStream(error) {
+    console.log(error);
+  }
+
+  useEffect(() => {
+    navigator.getUserMedia(constraints, successStream, failureStream);
+  });
+
+  return (
+    <div>
+      <canvas ref={canvasRef} hidden id="canvas" />
+      <video ref={videoRef} muted playsInline autoPlay />
+      <p>{ msg }</p>
+    </div>
+  );
 }
 
 export default QrCodeScanner;
