2d44fa5c4914616e011fa12a9bc4e5a33dfabe1d
Vincent Humeau <vincent.humeau@gmail.com>
2019-07-24 18:04:32 +0100

    Updates

--- precache-manifest.fd55d29dd0d695898b3a300f0319a428.js
+++ precache-manifest.fd55d29dd0d695898b3a300f0319a428.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "65ab032962c81caa890809a9aeb362da",
+    "revision": "c4f5a14d2704cb5cbcc8608c8c399b4c",
     "url": "/qr-code-generator/index.html"
   },
   {
-    "revision": "715922775427390d090a",
+    "revision": "2bdce0f3b942987edb6f",
     "url": "/qr-code-generator/static/css/main.02aa14b4.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/qr-code-generator/static/js/2.d293921f.chunk.js"
   },
   {
-    "revision": "715922775427390d090a",
-    "url": "/qr-code-generator/static/js/main.2cfc5e39.chunk.js"
+    "revision": "2bdce0f3b942987edb6f",
+    "url": "/qr-code-generator/static/js/main.53e3ce64.chunk.js"
   },
   {
     "revision": "eb5dffedbb6780615d15",
